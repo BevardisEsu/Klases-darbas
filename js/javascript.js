01c458879aca17d885e11eda78baf45968507073
Vilius Šeputis <vilius951@gmail.com>
2022-12-12 14:59:49 +0200

    Mano pirmasis komitas

---
+++ js/javascript.js
@@ -0,0 +1,35 @@
+let x = 5;
+let y = 6;
+let z = x+y;
+console .log(z);
+
+/* pirma užduotis
+let a = 1;
+const result = (a);
+const message = 'Kintamojo a reikšmė:';
+console.log(message, result); */
+
+
+/*antra užduotis
+let b= 'Vilius Šeputis';
+const result = (b);
+const c ='Mano vardas';
+console.log(c,b); */
+
+ // Trečia užduotis
+let metai ='1999';
+let menuo ='10';
+let diena ='09';
+m =metai % 10;
+me = menuo % 10;
+d = diena % 10;
+result = m + me + d;
+console .log(result);
+
+
+
+
+
+
+
+
